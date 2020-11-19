562c9bc3297e3d6501f3758463bd4add8552bfb6
hazen <hoyeungw@outlook.com>
2020-11-19 11:46:19 +0800

    init commit

---
+++ Spare.Deco/DecoCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace Spare.Deco {
+  public static class DecoCollection {
+    public static string DecoVector<T>(this T[] vec) {
+      var arr = vec.Select(x => x.ToString());
+      return "[" + string.Join(", ", arr) + "]";
+    }
+  }
+}
