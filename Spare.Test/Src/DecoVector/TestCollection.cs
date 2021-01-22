5b3786f92f45c5e75c3183344c6e804d093a0211
Hoyeung <hoyeungw@outlook.com>
2021-01-22 18:44:53 +0800

    minor fix

---
+++ Spare.Test/Src/DecoVector/TestCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using Spare.Deco;
+
+namespace Spare.Test.DecoVector {
+  [TestFixture]
+  public class TestCollection {
+    [Test]
+    public void Test1() {
+      var elements = new[] {1, 2, 3};
+      Console.WriteLine(elements.DecoVector());
+      Assert.True(true);
+    }
+  }
+}
