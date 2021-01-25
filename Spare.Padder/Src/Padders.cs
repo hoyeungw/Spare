5aab18ebebd359d722a8590f08d11fbac382720d
hoyeungw <hoyeungw@outlook.com>
2021-01-25 16:32:19 +0800

    add Spare.Padder

---
+++ Spare.Padder/Src/Padders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Veho.Matrix;
+using Veho.Vector;
+
+namespace Spare.Padder {
+  public static class Padders {
+    public static string[,] Padder(this string[,] matrix) {
+      var widths = matrix.MapColumns(
+        col => col.Fold((l, t) => Math.Max(l, t.Length), 0)
+      );
+      return matrix.Map((i, j, v) => v.PadLeft(widths[j]));
+    }
+  }
+}
