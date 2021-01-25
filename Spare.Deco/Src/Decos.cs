fa4f1d2b08b5ae816019d36119ffaa2f782701c7
hoyeungw <hoyeungw@outlook.com>
2021-01-25 16:32:14 +0800

    add Spare.Padder

---
+++ Spare.Deco/Src/Decos.cs
@@ -0,0 +1,16 @@
+﻿using Spare.Padder;
+using Veho.Matrix;
+using Veho.Vector;
+
+namespace Spare.Deco {
+  public static class Decos {
+    // return "[" + string.Join(", ", arr) + "]";
+    public static string Deco<T>(this T[] vector) => "[" + vector.Join(", ") + "]";
+
+    public static string Deco<T>(this T[,] matrix) {
+      var mx = matrix.Map(x => x.ToString());
+      var padded = mx.Padder();
+      return "[" + padded.MapRows(row => row.Join(",")).Join(",\n") + "]";
+    }
+  }
+}
--- Spare.Test/Src/DecoVector/TestCollection.cs
+++ Spare.Test/Src/DecoVector/TestCollection.cs
@@ -6,9 +6,9 @@ namespace Spare.Test.DecoVector {
   [TestFixture]
   public class TestCollection {
     [Test]
-    public void Test1() {
+    public void DecoVectorTest() {
       var elements = new[] {1, 2, 3};
-      Console.WriteLine(elements.DecoVector());
+      Console.WriteLine(elements.Deco());
       Assert.True(true);
     }
   }
