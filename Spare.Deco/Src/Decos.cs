6e864b9eebb5bd7f5eebf38f93238b43b6420939
hoyeungw <hoyeungw@outlook.com>
2021-01-25 16:50:56 +0800

    Deco for 2d-array can format none-zero-based 2d-array

--- Spare.Deco/Src/Decos.cs
+++ Spare.Deco/Src/Decos.cs
@@ -1,16 +1,25 @@
 ﻿using Spare.Padder;
+using Texting.Bracket;
 using Veho.Matrix;
 using Veho.Vector;
+using static Texting.Enums.Chars;
 
 namespace Spare.Deco {
   public static class Decos {
     // return "[" + string.Join(", ", arr) + "]";
-    public static string Deco<T>(this T[] vector) => "[" + vector.Join(", ") + "]";
+    public static string Deco<T>(this T[] vector) => vector.Length == 0
+      ? "[]"
+      : (SP + vector.Join(COSP) + COSP).Bracket();
 
-    public static string Deco<T>(this T[,] matrix) {
-      var mx = matrix.Map(x => x.ToString());
-      var padded = mx.Padder();
-      return "[" + padded.MapRows(row => row.Join(",")).Join(",\n") + "]";
+    public static string Deco<T>(this T[,] matrix, byte tab = 1) {
+      var tabSpace = new string(' ', tab * 2);
+      var (xlo, ylo) = (matrix.XLo(), matrix.YLo());
+      var head = xlo != 0 || ylo != 0 ? $"{xlo}-{ylo}-based".Parenth() + SP : VO;
+      var texts = (head.Length > 0)
+        ? matrix.ZeroOut(x => x.ToString())
+        : matrix.Map(x => x.ToString());
+      var padded = texts.Padder();
+      return head + (LF + padded.MapRows(row => tabSpace + row.Join(COSP)).Join(COLF) + COLF).Bracket();
     }
   }
 }
--- Spare.Test/Src/DecoVector/TestCollection.cs
+++ Spare.Test/Src/DecoVector/TestCollection.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using NUnit.Framework;
 using Spare.Deco;
+using Veho.Vector;
+using Veho.Matrix;
+using Inits = Veho.Matrix.Inits;
 
 namespace Spare.Test.DecoVector {
   [TestFixture]
@@ -8,6 +11,18 @@ namespace Spare.Test.DecoVector {
     [Test]
     public void DecoVectorTest() {
       var elements = new[] {1, 2, 3};
+      Console.WriteLine(elements.Deco());
+      Assert.True(true);
+    }
+    [Test]
+    public void DecoMatrixTest() {
+      var elements = Inits.Init(3, 5, (i, j) => Math.Pow(10, i * 2 + 1) + (j + 1));
+      Console.WriteLine(elements.Deco());
+      Assert.True(true);
+    }
+    [Test]
+    public void DecoMatrix1BTest() {
+      var elements = Base1.M1B<int>(3, 5);
       Console.WriteLine(elements.Deco());
       Assert.True(true);
     }
