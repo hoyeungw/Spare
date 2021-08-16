b54caed2e09735365dd87e767cecac2ad0b096f1
hoyeungw <hoyeungw@outlook.com>
2021-08-16 11:03:34 +0800

    rewrite Deco for Vector

---
+++ Spare.Deco/Src/DecoVector.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Palett.Fluos;
+using Palett.Types;
+using Texting.Enums;
+using Texting.Joiner;
+using Typen;
+using Veho.Vector;
+using static Texting.Enums.Strings;
+
+namespace Spare {
+  public static partial class Decos {
+    public static string Deco<T>(
+      this T[] vector,
+      string delim = COSP,
+      (Preset, Preset)? presets = null,
+      params Effect[] effects
+    ) {
+      if (!vector.Any()) return "[]";
+      var textVector = typeof(T) == typeof(string) ? vector.Cast<T[], string[]>() : vector.Map(Conv.ToStr);
+      if (presets != null) textVector = textVector.Fluo(presets.Value, effects);
+      return textVector.ContingentLines(delim, brac: Brac.BRK);
+    }
+  }
+}
