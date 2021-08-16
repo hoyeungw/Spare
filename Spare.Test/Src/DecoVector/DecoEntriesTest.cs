0fa1c3a664850c9960efa161b9ff4304315ac603
hoyeungw <hoyeungw@outlook.com>
2021-08-16 12:12:28 +0800

    minor fix

---
+++ Spare.Test/Src/DecoVector/DecoEntriesTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace Spare.Test.DecoVector {
+  [TestFixture]
+  public class DecoEntriesTest {
+    [Test]
+    public void DecoEntriesTestAlpha() {
+      Candidates.CarPlants.ToArray().DecoEntries().Logger();
+    }
+  }
+}
