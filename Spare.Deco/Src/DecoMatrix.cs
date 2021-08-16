c018a04a8dd1e8206a34d645f2112fc4f5e7e46b
hoyeungw <hoyeungw@outlook.com>
2021-08-16 11:04:06 +0800

    rewrite Deco for Matrix

---
+++ Spare.Deco/Src/DecoMatrix.cs
@@ -0,0 +1,31 @@
+﻿using Palett.Fluos;
+using Palett.Types;
+using Spare.Padder;
+using Texting.Joiner;
+using Typen;
+using Veho.Matrix;
+using Veho.Rows;
+using Veho.Types;
+using static Texting.Enums.Strings;
+
+namespace Spare {
+  public static partial class Decos {
+    public static string Deco<T>(
+      this T[,] matrix,
+      byte tab = 1,
+      bool hasAnsi = false,
+      Operated orient = Operated.Rowwise,
+      (Preset, Preset)? presets = null,
+      params Effect[] effects
+    ) {
+      if (!matrix.Any()) return "[]";
+      var textMatrix = matrix.Map(Conv.ToStr);
+      if (presets != null && (hasAnsi = true)) {
+        textMatrix = textMatrix.Fluo(orient, presets.Value, effects);
+      }
+      return textMatrix.Padder(hasAnsi)
+                       .MapRows(row => row.Join(COSP))
+                       .ContingentLines(delim: COLF, level: tab);
+    }
+  }
+}
