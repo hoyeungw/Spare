99aee793b7e6f5420c377228586e1057ac753906
hoyeungw <hoyeungw@outlook.com>
2021-08-16 12:11:55 +0800

    add DecoEntries

--- Spare.Deco/Src/DecoEntries.cs
+++ Spare.Deco/Src/DecoEntries.cs
@@ -1,5 +1,38 @@
-﻿namespace Spare {
-  public class DecoEntries {
-    
+﻿using System.Collections.Generic;
+using System.Linq;
+using Palett.Fluos;
+using Palett.Types;
+using Spare.Padder;
+using Texting.Enums;
+using Texting.Joiner;
+using Typen;
+using Veho;
+using Veho.Vector;
+
+namespace Spare {
+  public static class DecoEnt {
+    public static string DecoEntries<TK, TV>(
+      this (TK key, TV value)[] entries,
+      byte tab = 1,
+      bool hasAnsi = false,
+      string delim = Strings.RTSP,
+      (Preset, Preset)? presets = null,
+      params Effect[] effects
+    ) {
+      if (!entries.Any()) return "[]";
+      var (keys, values) = entries.Unwind();
+      string[]
+        textKeys = keys.Map(Conv.ToStr),
+        textValues = values.Map(Conv.ToStr);
+      if (presets.HasValue && (hasAnsi = true)) {
+        textKeys = textKeys.Fluo(presets.Value, effects);
+        textValues = textValues.Fluo(presets.Value, effects);
+      }
+      textKeys = textKeys.LPadder(hasAnsi);
+      textValues = textValues.RPadder(hasAnsi);
+
+      var textEntries = textKeys.Zip(textValues, (k, v) => k + delim + v);
+      return textEntries.ContingentLines(delim: Strings.COLF, level: tab, Brac.BRK);
+    }
   }
 }
