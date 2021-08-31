91d012e90af798d5ca2c5340c034cb4cf7846626
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:53:34 +0800

    add MaxLength for IReadOnlyList<T>

--- Spare.Padder/Src/Padders.cs
+++ Spare.Padder/Src/Padders.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Texting.Lange;
 using Texting.Padder;
 using Veho.Columns;
@@ -6,6 +8,17 @@ using Veho.Matrix;
 using Veho.Vector;
 
 namespace Spare.Padder {
+  public static class LengthUtil {
+    public static int MaxLength(this IReadOnlyList<string> column, bool ansi = true) {
+      var lange = Langes.ToLange(ansi);
+      return column.Max(tx => lange(tx));
+    }
+    public static int MaxLength<T>(this IReadOnlyList<T> column, Func<T, string> selector, bool ansi = true) {
+      var lange = Langes.ToLange(ansi);
+      return column.Max(x => lange(selector(x)));
+    }
+  }
+
   public static class Padders {
     public static string[,] Padder(this string[,] matrix, bool ansi = true) {
       var lange = Langes.ToLange(ansi);
@@ -15,16 +28,16 @@ namespace Spare.Padder {
       );
       return matrix.Map((i, j, v) => padder(v, widths[j]));
     }
-    public static string[] RPadder(this string[] column, bool ansi = true) {
-      var lange = Langes.ToLange(ansi);
+    public static string[] RPadder(this IReadOnlyList<string> column, bool ansi = true) {
       var padder = PadFactory.ToRPad(' ', ansi);
-      var width = column.Fold((max, tx) => Math.Max(max, lange(tx)), 0);
+      var width = column.MaxLength(ansi);
       return column.Map(word => padder(word, width));
     }
-    public static string[] LPadder(this string[] column, bool ansi = true) {
-      var lange = Langes.ToLange(ansi);
+    public static string[] LPadder(this IReadOnlyList<string> column, bool ansi = true) {
       var padder = PadFactory.ToLPad(' ', ansi);
-      var width = column.Fold((max, tx) => Math.Max(max, lange(tx)), 0);
+      var width = column.MaxLength(ansi);
+      // var lange = Langes.ToLange(ansi);
+      // var width = column.Fold((max, tx) => Math.Max(max, lange(tx)), 0);
       return column.Map(word => padder(word, width));
     }
   }
