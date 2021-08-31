658a65fc6f380ccf1648c8c6acd1f4c529527af5
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:52:54 +0800

    unified Deco for List<T> and T[] by using IReadOnlyList<T>

--- Spare.Deco/Src/Decos.Vector.cs
+++ Spare.Deco/Src/Decos.Vector.cs
@@ -1,22 +1,25 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Palett.Fluos;
 using Palett.Types;
 using Texting.Enums;
 using Texting.Joiner;
 using Typen;
-using Veho.Vector;
+using Veho.Sequence;
 using static Texting.Enums.Strings;
 
 namespace Spare {
   public static partial class Decos {
     public static string Deco<T>(
-      this T[] vector,
+      this IReadOnlyList<T> vector,
       string delim = COSP,
       (Preset, Preset)? presets = null,
       params Effect[] effects
     ) {
       if (!vector.Any()) return "[]";
-      var textVector = typeof(T) == typeof(string) ? vector.Cast<T[], string[]>() : vector.Map(Conv.ToStr);
+      var textVector = typeof(T) == typeof(string)
+        ? vector.Cast<IReadOnlyList<T>, IReadOnlyList<string>>()
+        : vector.Map(Conv.ToStr);
       if (presets != null) textVector = textVector.Fluo(presets.Value, effects);
       return textVector.ContingentLines(delim, brac: Brac.BRK);
     }
