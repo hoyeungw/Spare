3f140aea968c3d106124d6e2653f6ff994e0b675
hoyeungw <hoyeungw@outlook.com>
2021-09-02 14:54:33 +0800

    add Deco for mutable matrix

--- Spare.Deco/Src/Decos.MutableMatrix.cs
+++ Spare.Deco/Src/Decos.MutableMatrix.cs
@@ -1,28 +1,19 @@
-﻿// using System.Collections.Generic;
-// using Palett.Types;
-// using Typen;
-// using Veho.Mutable.Matrix;
-// using Veho.Mutable;
-// using Veho.Types;
-//
-// namespace Spare {
-//   public static partial class Decos {
-//     public static string Deco<T>(
-//       this List<List<T>> matrix,
-//       byte tab = 1,
-//       bool hasAnsi = false,
-//       Operated orient = Operated.Rowwise,
-//       (Preset, Preset)? presets = null,
-//       params Effect[] effects
-//     ) {
-//       if (!matrix.Any()) return "[]";
-//       var textMatrix = matrix.Map(Conv.ToStr);
-//       if (presets != null && (hasAnsi = true)) {
-//         textMatrix = textMatrix.Fluo(orient, presets.Value, effects);
-//       }
-//       return textMatrix.Padder(hasAnsi)
-//                        .MapRows(row => row.Join(COSP))
-//                        .ContingentLines(delim: COLF, level: tab);
-//     }
-//   }
-// }
+﻿using System.Collections.Generic;
+using Palett.Types;
+using Veho.Mutable.Matrix;
+using Veho.Types;
+
+namespace Spare {
+  public static partial class Decos {
+    public static string Deco<T>(
+      this IReadOnlyList<IReadOnlyList<T>> matrix,
+      byte tab = 1,
+      bool hasAnsi = false,
+      Operated orient = Operated.Rowwise,
+      (Preset, Preset)? presets = null,
+      params Effect[] effects
+    ) {
+      return matrix.ToMatrix().Deco(tab, hasAnsi, orient, presets, effects);
+    }
+  }
+}
