0c1491158f9a99ee0b5e7e3878e533ce10be1a61
hoyeungw <hoyeungw@outlook.com>
2024-11-30 07:35:33 +0800

    update dependencies and vi

--- Spare.Test/Src/TupleTests.cs
+++ Spare.Test/Src/TupleTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Veho;
+using Veho.Tuple;
 
 namespace Spare.Test {
   [TestFixture]
@@ -8,7 +9,7 @@ namespace Spare.Test {
     public void TestAlpha() {
       var tuple = (0, 0, 0);
       tuple.Item2 = 1;
-      tuple.ToList().Deco().Logger();
+      tuple.List().Deco().Logger();
     }
   }
 }
